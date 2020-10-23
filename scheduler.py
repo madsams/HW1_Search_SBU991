e91494030d9a8bc57ed3dde9094dd6c1dbfc52a1
Mohammad Seyfayi <madsams024@gmail.com>
2020-10-23 22:30:28 +0330

    first run added

--- scheduler.py
+++ scheduler.py
@@ -4,6 +4,7 @@ from scrapy.utils.project import get_project_settings
 
 process = CrawlerProcess(get_project_settings())
 scheduler = TwistedScheduler()
+process.crawl('dollar')  # run for the first time
 scheduler.add_job(process.crawl, 'interval', args=['dollar'], seconds=24 * 60 * 60 * 60)
 scheduler.start()
 process.start(False)
